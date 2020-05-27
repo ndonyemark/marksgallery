2a7241db4d767393ba641ed8e607b6eb0303d99f
Ndonye Mark <ndonyemark@gmail.com>
2020-05-28 00:10:11 +0300

    remove whitenoise storage

--- gallery/settings.py
+++ gallery/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -139,3 +140,7 @@ STATICFILES_DIRS = [
 # user_defined
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
