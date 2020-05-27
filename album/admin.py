e69f6fb1498b52c05f3ac831b2d2462290cdb1a0
Ndonye Mark <ndonyemark@gmail.com>
2020-05-27 13:42:07 +0300

    add the admin data

--- album/admin.py
+++ album/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Location, Image, Category
 
 # Register your models here.
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal = ('image_category',)
+
+admin.site.register(Location)
+admin.site.register(Image, ImageAdmin)
+admin.site.register(Category)
