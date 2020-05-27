c62d0a3d53a3fe10dac8ab12a2903f0faa5cdfaf
Ndonye Mark <ndonyemark@gmail.com>
2020-05-27 13:52:37 +0300

    repair .ENV

--- gallery/urls.py
+++ gallery/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('snap.urls'))
+    path('', include('album.urls'))
 ]
 
 urlpatterns+=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
