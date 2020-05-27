a82e5364af12ef21d46c73e5d862460cb4608d7e
Ndonye Mark <ndonyemark@gmail.com>
2020-05-27 13:50:58 +0300

    repair settings.py

--- gallery/settings.py
+++ gallery/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY') 
-
+print(SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG")
 
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    'album.app.AlbumConfig',
+    'album',
     'bootstrap4'
 
 ]
