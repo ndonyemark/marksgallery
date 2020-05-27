51cc966d43ca2257550af0b4470ae31764991c14
Ndonye Mark <ndonyemark@gmail.com>
2020-05-27 13:39:09 +0300

    add all view files

--- album/views.py
+++ album/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Image
 
 # Create your views here.
+
+def index(request):
+    images = Image.get_all_images()
+    title = "View all posted pictures"
+    return render(request, 'index.html', {'title': title, "images": images})
+
+def search(request):
+    if 'search' in request.GET and request.GET['search']:
+        search_term = request.GET.get('search')
+        searched_categories = Image.search_by_category(search_term)
+        message = f'{search_term}'
+        return render(request, 'search.html', {'message': message, 'categories': searched_categories})
+    
+    else:
+        message = "Sorry but none of your images has such a term"
+        return render(request, 'search.html', {'message': message})
+    
